89a641ac76e8ace49fed62060c47b7d14bafd6c2
Mickael Moreira <dopidot@gmail.com>
2020-05-11 18:04:14 +0200

    Add entete method in C++

--- Machine_Learning_Lib/Machine_Learning_Lib/Source.cpp
+++ Machine_Learning_Lib/Machine_Learning_Lib/Source.cpp
@@ -15,4 +15,39 @@ extern "C"
 	{
 		return x * y;
 	}
+
+	DLLEXPORT double linear_model_predict_regression(double *model,
+		double *inputs, int inputs_size)
+	{
+		// TODO
+		return 0.0;
+	}
+
+	DLLEXPORT double linear_model_predict_classification(double *model,
+		double *inputs, int inputs_size)
+	{
+		return linear_model_predict_regression(model, inputs, inputs_size) >= 0 ?
+			1.0 : -1.0;
+	}
+
+	DLLEXPORT void linear_model_train_classification(double *model,
+		double* dataset_inputs, int dataset_length, int inputs_size,
+		double* dataset_expected_outputs, int outputs_size,
+		int interations_count, float alpha)
+	{
+		// TODO : Train Rosenblatt
+	}
+
+	DLLEXPORT void linear_model_train_regression(double *model,
+		double* dataset_inputs, int dataset_length, int inputs_size,
+		double* dataset_expected_outputs, int outputs_size/*,
+		int interations_count, float alpha*/)
+	{
+		// TODO : Train PseudoInverse moore penrose
+	}
+
+	DLLEXPORT void linear_model_delete(double *model)
+	{
+		// TODO : Delete XXXX ?
+	}
 }
