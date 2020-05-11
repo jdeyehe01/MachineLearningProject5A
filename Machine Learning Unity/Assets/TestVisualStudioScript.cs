ed70ad07ede96cd1477d59f296016c2ad8236e2c
Mickael Moreira <dopidot@gmail.com>
2020-05-11 17:34:59 +0200

    Init Unity 3D project

---
+++ Machine Learning Unity/Assets/TestVisualStudioScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVisualStudioScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("With Visual Studio DLL : ");
+        Debug.Log(VisualStudioLibWrapper.my_add(42, 51));
+        Debug.Log(VisualStudioLibWrapper.my_mul(2, 3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Machine Learning Unity/Assets/VisualStudioLibWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+public static class VisualStudioLibWrapper
+{
+    [DllImport("Machine_Learning_Lib")]
+    public static extern int my_add(int x, int y);
+
+    [DllImport("Machine_Learning_Lib")]
+    public static extern int my_mul(int x, int y);
+}
