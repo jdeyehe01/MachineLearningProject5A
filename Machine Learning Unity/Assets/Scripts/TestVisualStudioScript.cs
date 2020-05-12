4a340e0498f9355a5dbb6f4cf030b1fbb37bd3a4
Tako Guillaume <guillaumetako@yahoo.fr>
2020-05-12 19:24:40 +0200

    Modif Script Unity

--- Machine Learning Unity/Assets/Scripts/TestVisualStudioScript.cs
+++ Machine Learning Unity/Assets/Scripts/TestVisualStudioScript.cs
@@ -1,20 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class TestVisualStudioScript : MonoBehaviour
 {
+    public Transform[] trainSpheresTransforms;
+    public Transform[] testSpheresTransforms;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("With Visual Studio DLL : ");
-        Debug.Log(VisualStudioLibWrapper.my_add(42, 51));
-        Debug.Log(VisualStudioLibWrapper.my_mul(2, 3));
+        double[] model = CreateModel();
+        /*foreach ( var testSpheresTransform in testSpheresTransforms)
+        {
+            testSpheresTransform.position += Vector3.up * 10;
+        }*/
+        Predict(model);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    double[] CreateModel()
+    {
+        Debug.Log("Create Model");
+        return VisualStudioLibWrapper.linear_model_create(10);
+    }
+
+    void Predict(double[] model)
+    {
+        Debug.Log("Predict Model");
+        /*foreach ( var testSpheresTransform in testSpheresTransforms)
+        {
+        }*/
+
+
+        //double result = VisualStudioLibWrapper.linear_model_predict_regression(model, testSpheresTransforms, 121);
+        //Debug.Log("Result Predict Model =  " + result);
+    }
+
+    void Train()
+    {
+        Debug.Log("Train Model");
     }
 }
--- Machine Learning Unity/Assets/Scripts/VisualStudioLibWrapper.cs
+++ Machine Learning Unity/Assets/Scripts/VisualStudioLibWrapper.cs
@@ -3,8 +3,20 @@
 public static class VisualStudioLibWrapper
 {
     [DllImport("Machine_Learning_Lib")]
-    public static extern int my_add(int x, int y);
+    public static extern double[] linear_model_create(int dim_size);
 
     [DllImport("Machine_Learning_Lib")]
-    public static extern int my_mul(int x, int y);
+    public static extern double linear_model_predict_regression(double[] model, double[] inputs, int inputs_size);
+
+    [DllImport("Machine_Learning_Lib")]
+    public static extern double linear_model_predict_classification(double[] model, double[] inputs, int inputs_size);
+
+    [DllImport("Machine_Learning_Lib")]
+    public static extern void linear_model_train_classification(double[] model, double[] dataset_inputs, int dataset_length, int inputs_size,
+        double[] dataset_expected_outputs, int outputs_size, int interations_count, double alpha);
+
+    [DllImport("Machine_Learning_Lib")]
+    public static extern void linear_model_train_regression(double[] model, double[] dataset_inputs, int dataset_length, int inputs_size, double[] dataset_expected_outputs, int outputs_size);
+
+    //double** => Marshal.Copy(IntPtr, Double[], Int32, Int32)
 }
