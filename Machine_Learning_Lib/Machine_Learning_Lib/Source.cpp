2ea59d80cc9983ad68da29dd7418d6a1390311cb
DEYEHE <jdeyehe@gmail.com>
2020-05-15 00:06:56 +0200

    mlp

--- Machine_Learning_Lib/Machine_Learning_Lib/Source.cpp
+++ Machine_Learning_Lib/Machine_Learning_Lib/Source.cpp
@@ -1,4 +1,4 @@
-#if _WIN32
+ï»¿#if _WIN32
 #define DLLEXPORT __declspec(dllexport)
 #else
 #define DLLEXPORT
@@ -8,12 +8,60 @@
 #include <Eigen/Dense>
 #include <stdlib.h>
 #include <iostream>
+#include <math.h>
+#include <algorithm>
 
 using namespace Eigen;
 
 
 extern "C"
 {
+
+
+	// faire un new de ce truc 
+	struct MLP {
+		int* npl;
+		int npl_size;
+		double*** w;// valeur des poids [0] =layer (l), [1] = i, [2] = j 
+		double** x; // enregistrement des valeurs x rï¿½sultant de la propagation 
+		double** deltas; //  les deltas a stocker dans l'apprentissage 
+	};
+
+	void mlp_propagation(MLP* model, double* inputs, bool regression) {
+
+		for (auto j = 1; j < model->npl[0] + 1; j++) {
+			model->x[0][j] = inputs[j - 1];
+		}
+
+		for (auto l = 1; l < model->npl_size; l++) {
+			for (auto j = 1; j < model->npl[l] + 1; j++) {
+
+				auto sum = 0.0;
+
+				for (auto i = 0; i < model->npl[l - 1] + 1) {
+					sum += model->w[l][i][j] * model->x[l - 1][i];
+				}
+
+				model->x[l][j] = ((l == model->npl_size - 1) && regression) ? sum : tanh(sum);
+			}
+		}
+	}
+
+
+	double* mlp_propagation_and_extract_result(MLP* model, double* inputs, bool regression) {
+		mlp_propagation(model, inputs, false);
+		auto result = new double[model->npl[model->npl_size]];
+
+		for (auto j = 1; j < model->npl[model->npl_size - 1] + 1; j++) {
+
+			result[j - 1] = model->x[model->npl_size - 1][j];
+		}
+
+		return result;
+	}
+
+
+
 	DLLEXPORT double* linear_model_create(int dim_size)
 	{
 		auto tab = new double[dim_size + 1];
@@ -26,7 +74,7 @@ extern "C"
 		return tab;
 	}
 
-	DLLEXPORT double linear_model_predict_regression(double *model,double *inputs, int inputs_size)
+	DLLEXPORT double linear_model_predict_regression(double* model, double* inputs, int inputs_size)
 	{
 		// model W => { x0, x1, x2 }
 		// inputs X => { x1, x2 }
@@ -40,29 +88,29 @@ extern "C"
 		return result + model[0];
 	}
 
-	DLLEXPORT double linear_model_predict_classification(double *model,double *inputs, int inputs_size)
+	DLLEXPORT double linear_model_predict_classification(double* model, double* inputs, int inputs_size)
 	{
 		// Meme chose que la regression mais avec un fonction signe
 		return linear_model_predict_regression(model, inputs, inputs_size) >= 0 ? 1.0 : -1.0;
 	}
 
-	DLLEXPORT void linear_model_train_classification(double *model,double* dataset_inputs, int dataset_length, int inputs_size,double* dataset_expected_outputs,
-		int interations_count,double alpha)
+	DLLEXPORT void linear_model_train_classification(double* model, double* dataset_inputs, int dataset_length, int inputs_size, double* dataset_expected_outputs,
+		int interations_count, double alpha)
 	{
 		for (int i = 0; i < interations_count; i++) {
 
 			int indexRand = rand() % dataset_length;
 			int pos = indexRand * inputs_size;
-			double g_x_k = linear_model_predict_classification(model, &dataset_inputs[pos] , inputs_size);
+			double g_x_k = linear_model_predict_classification(model, &dataset_inputs[pos], inputs_size);
 			double grad = alpha * (dataset_expected_outputs[indexRand] - g_x_k);
 			model[0] += grad * 1;
 			for (int k = 0; k < inputs_size; k++) {
-				model[k + 1] += grad * dataset_inputs[pos+k];
+				model[k + 1] += grad * dataset_inputs[pos + k];
 			}
 		}
 	}
 
-	DLLEXPORT void linear_model_train_regression(double *model, double* dataset_inputs, int dataset_length, int inputs_size, double* dataset_expected_outputs)
+	DLLEXPORT void linear_model_train_regression(double* model, double* dataset_inputs, int dataset_length, int inputs_size, double* dataset_expected_outputs)
 	{
 		// TODO : Train PseudoInverse moore penrose
 		MatrixXd x(dataset_length, inputs_size + 1);
@@ -73,7 +121,7 @@ extern "C"
 			x(i, 0) = 1;
 
 			for (int k = 1; k < (inputs_size + 1); k++) {
-				x(i, k) = dataset_inputs[i * inputs_size + (k-1)];
+				x(i, k) = dataset_inputs[i * inputs_size + (k - 1)];
 			}
 		}
 
@@ -83,76 +131,154 @@ extern "C"
 		}
 	}
 
-	DLLEXPORT void linear_model_delete(double *model)
+	DLLEXPORT void linear_model_delete(double* model)
 	{
 		delete model;
 	}
 
-	// faire un new de ce truc 
-	struct MLP {
-		int* npl; 
-		int npl_size; 
-		double*** w;// valeur des poids [0] =layer (l), [1] = i, [2] = j 
-		double** x; // enregistrement des valeurs x résultant de la propagation 
-		double** deltas; //  les deltas a stocker dans l'apprentissage 
-	};
 
-	// mlp_model_create([2, 3, 4, 1], 4); // a récupérer avec un IntPtr
+
+	// mlp_model_create([2, 3, 4, 1], 4); // a rï¿½cupï¿½rer avec un IntPtr
 	DLLEXPORT struct MLP* mlp_model_create(int* npl, int npl_size)
-	{ 
+	{
 
+		auto model = new MLP();
 
-		// npl tableau d'entier ( neurone per layer )
-		// [2, 3, 4, 1]
-		// 2 =>  Deux entrée
-		// 3, 4 => deux couches cachée avec respectivement 3 et 4 neurones sans compter le neurone fictive 1
-		// 1 => une sortie 
+		model->npl = new int[npl_size];
 
-		// np)l_size = la taille du tableau
+		for (auto i = 0; i < npl_size; i++) {
+			model->npl[i] = npl_size;
+		}
+		model->npl_size = npl_size;
 
+		model->w = new double** [npl_size];
 
-		// Création de la structure mlp pour sauvegarder tout ce dont on a besoin 
+		for (auto l = 1; l < npl_size; l++) {
 
+			model->w[l] = new double* [npl[l - 1] + 1];
 
+			for (auto i = 1; i < npl[l - 1] + 1; i++) {
 
-		// Nombre d'entrée + 1 pour l'entrée fictive
-		/* Seule chose qui change entre le mlp de classification pour la régression et pour la classification c
-		*/
+				model->w[l][i] = new double[npl[l] + 1];
 
-		/*
-			w[l][i][j]
-			l => layer neurone arrive /-> taille nb layer
-			i => position i neurone départ /-> taille nb neurones
-			j => position j neurone arrive /-> taille nb neurones
-		*/
-		double*** w_ptr = new double** [npl_size];
-		for (int layer = 1; layer < npl_size; ++layer) {
-			w_ptr[layer] = new double* [npl[layer -1] + 1]; // nombre de neurone du layer l + neurone fictif
-			
-			for (int neurone_i = 0; neurone_i < npl[layer -1 ] + 1; ++neurone_i) {
-				w_ptr[layer][neurone_i] = new double[npl[layer] + 1]; // nombre de neurone du layer l + neurone fictif
-
-				for (int neurone_j = 0; neurone_j < npl[layer] + 1; ++neurone_j) {
-					w_ptr[layer][neurone_i][neurone_j] = ((double) rand()) / RAND_MAX * 2.0 - 1.0;
+				for (auto j = 1; j < npl[l] + 1; j++) {
+					model->w[l][i][j] = ((double)rand()) / RAND_MAX * 2.0 - 1.0;
 				}
 			}
 		}
 
 
-		double** x_ptr = new double* [npl_size]; // x[L][J]
-		double** deltas_ptr = new double* [npl_size];
+		model->x = new double* [npl_size];
+		model->deltas = new double* [npl_size];
+
 		for (auto l = 0; l < npl_size; l++) {
-			//model->x[l] = new double[npl[l] + 1];
+			model->x[l] = new double[npl[l] + 1];
+			model->x[l][0] = 1.0;
+			model->deltas[l] = new double[npl[l] + 1];
 		}
 
-		MLP* model = new MLP;
-
-		model->npl = npl;
-		model->npl_size = npl_size;
-		model->w = w_ptr;
-		model->x = x_ptr;
-		model->deltas = deltas_ptr;
-
 		return model;
+	}
+
+
+	DLLEXPORT double* mlp_model_predict_regression(struct MLP* model, double* inputs) {
+		return mlp_propagation_and_extract_result(model, inputs, true);
+	}
+
+
+
+	DLLEXPORT double* mlp_model_predict_classification(struct MLP* model, double* inputs) {
+
+		return mlp_propagation_and_extract_result(model, inputs, false);
+	}
+
+
+
+
+
+	DLLEXPORT void mlp_model_train_classification(struct MLP* model, double* dataset_inputs, int dataset_length, int inputs_size, double* dataset_expected_outputs,
+		int outputs_size, int interations_count, double alpha) {
+
+		for (auto it = 0; it < interations_count; it++)
+		{
+			auto k = floor(((double)std::min(rand(), RAND_MAX - 1) / RAND_MAX * dataset_length);
+			auto inputs = dataset_inputs + k * inputs_size;
+			auto expected_outputs = dataset_expected_outputs + k * outputs_size;
+
+			mlp_propagation(model, inputs, true);
+
+			for (auto j = 1; j < model->npl[model->npl_size - 1] + 1; j++) {
+				model->deltas[model->npl_size - 1][j] = (1 - pow(model->x[model->npl_size - 1][j], 2)) * (model->x[model->npl_size - 1][j] - expected_outputs[j - 1]);
+			}
+
+
+			for (auto l = model->npl_size - 1; l >= 2; l--)
+			{
+				for (auto i = 1; i < model->npl[l - 1] + 1; i++) {
+					auto sum = 0.0;
+					for (auto j = 1; j < model->npl[l] + 1; j++)
+					{
+						sum += model->w[l][i][j] * model->deltas[l][j];
+					}
+					model->deltas[l - 1][i] = (1 - pow(model->x[l - 1][i], 2)) * sum;
+				}
+			}
+
+			for (auto l = 1; l < model->npl_size; l++) 
+			{
+				for (auto i = 0; i < model->npl[l] + 1; i++) 
+				{
+					for (auto j = 1; j < model->npl[l] + 1; j++) 
+					{
+						model->w[l][i][j] -= alpha * model->x[l - 1][i] * model->deltas[l][j];
+					}
+				}
+			}
+
+		}
+	}
+
+
+	DLLEXPORT void mlp_model_train_regression(struct MLP* model, double* dataset_inputs, int dataset_length, int inputs_size, double* dataset_expected_outputs,
+		int outputs_size, int interations_count, double alpha)
+	{
+		for (auto it = 0; it < interations_count; it++)
+		{
+			auto k = floor(((double)std::min(rand(), RAND_MAX - 1) / RAND_MAX * dataset_length);
+			auto inputs = dataset_inputs + k * inputs_size;
+			auto expected_outputs = dataset_expected_outputs + k * outputs_size;
+
+			mlp_propagation(model, inputs, false);
+
+			for (auto j = 1; j < model->npl[model->npl_size - 1] + 1; j++) {
+				model->deltas[model->npl_size - 1][j] = (1 - pow(model->x[model->npl_size - 1][j], 2)) * (model->x[model->npl_size - 1][j] - expected_outputs[j - 1]);
+			}
+
+
+			for (auto l = model->npl_size - 1; l >= 2; l--)
+			{
+				for (auto i = 1; i < model->npl[l - 1] + 1; i++) {
+					auto sum = 0.0;
+					for (auto j = 1; j < model->npl[l] + 1; j++)
+					{
+						sum += model->w[l][i][j] * model->deltas[l][j];
+					}
+					model->deltas[l - 1][i] = (1 - pow(model->x[l - 1][i], 2)) * sum;
+				}
+			}
+
+			for (auto l = 1; l < model->npl_size; l++)
+			{
+				for (auto i = 0; i < model->npl[l] + 1; i++)
+				{
+					for (auto j = 1; j < model->npl[l] + 1; j++)
+					{
+						model->w[l][i][j] -= alpha * model->x[l - 1][i] * model->deltas[l][j];
+					}
+				}
+			}
+
+		}
+
 	}
 }
